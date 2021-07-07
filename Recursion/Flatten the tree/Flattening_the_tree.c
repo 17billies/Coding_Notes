803cb9e24aeef7dfc19681b01824ed54ac9a8fd5
Kitty Chao <kittychao520@gmail.com>
2021-07-07 22:47:43 +0800

    #1

--- Recursion/Flatten the tree/Flattening_the_tree.c
+++ Recursion/Flatten the tree/Flattening_the_tree.c
@@ -1,37 +1,22 @@
 //11662
 #include<stdio.h>
-void find_seq(int *A,int cur,int n);
-int B[2050]={0};
-int k=1;
+void find_seq(int *A, int cur, int n);
+int B[2050] = {0};
+int k = 1;
 int main(){
-    int A[2050]={0};
+    int A[2050] = {0};
     int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;++i){
-        scanf("%d",&A[i]);
-    }
-    find_seq(A,1,n);
-    for(int i=1;i<=n;++i){
-        printf("%d%c",B[i],' '*(i!=n)+'\n'*(i==n));
-   }
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i) scanf("%d" ,&A[i]);
+    find_seq(A, 1, n);
+    for(int i = 1; i <= n; ++i) printf("%d%c", B[i], ' '*(i != n)+'\n'*(i == n));
     return 0;
 }
 
-void find_seq(int *A,int cur,int n){
-    if(A[cur]==0) return;
-    
-    find_seq(A,2*cur,n);
-    B[k]=A[cur];
+void find_seq(int *A, int cur, int n){
+    if(A[cur] == 0) return; //means the index is empty
+    find_seq(A, 2*cur, n);
+    B[k] = A[cur];
     k++;
-    if(A[2*cur+1]!=0){
-        find_seq(A,2*cur+1,n);
-        return;
-    }
-    else return;
-<<<<<<< HEAD
+    if(A[2*cur+1] != 0) find_seq(A, 2*cur+1, n);  
 }
-=======
-    
-
-}
->>>>>>> 240260825243fb90f7dd71a2202d6820f8967392
